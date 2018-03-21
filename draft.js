a8cb73c9a7625be2c6488e2045557c02ad345459
wuyizhe0731 <wuyizhe528@gmail.com>
2018-03-21 21:37:41 +0000

    first commit

---
+++ draft.js
@@ -0,0 +1,11 @@
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+urlDatabase["9sm5xK"] = "abc.com"
+
+console.log(urlDatabase)
+
+
+		// urls that wants to delete:<br>
+		// <input type="text" name="id"> <br>
